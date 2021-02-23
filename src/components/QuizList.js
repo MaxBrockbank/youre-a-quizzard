5916d8e2916f6915621b907fbfcb7106ea643c1a
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-23 10:51:18 -0800

    Add to quiz list structure.
    Co-authored-by: Callie Stump <callie@stu.mp>
    o-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>

--- src/components/QuizList.js
+++ src/components/QuizList.js
@@ -1,2 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Quiz from './Quiz';
+import { useSelector } from 'react-redux';
+import {isLoaded, useFirestoreConnect } from 'react-redux-firebase';
 
+function QuizList(props){
+  useFirestoreConnect([
+    { collection: 'quizzes' }
+  ]);
+
+  const quizzes = useSelector(state => state.firestore.ordered.quizzes);
+
+  if (isLoaded(quizzes)) {
+    return(
+      <>
+        <hr/>
+        {quizzes.map((quiz) => {
+          return <Quiz
+          whenQuizClicked = {props.onQuizSelection}
+          quizName={quiz.quizName}
+          description={quiz.description}
+          question1= {quiz.question1}
+          question2= {quiz.question2}
+          question3= {quiz.question3}
+          question4= {quiz.question4}
+          />
+        })}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h3>No Quizzes Available</h3>
+      </>
+    )
+  }
+
+}
+QuizList.propTypes = {
+  onQuizSelection: PropTypes.func
+}
+
+export default QuizList;
